7e7991a65ca089c6c57d53624d3a95a4cff44674
masterxample <marc.fussel@gmail.com>
2026-08-23 21:47:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// Statische Dateien bereitstellen
-app.use(express.static(__dirname + '/public'));
+// Alle statischen Dateien direkt aus dem Hauptverzeichnis servieren
+app.use(express.static(__dirname));
 
-// Direkte Auslieferung der index.html für die Hauptseite
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// Direktes Ausliefern der index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
